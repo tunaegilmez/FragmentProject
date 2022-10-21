1f946a8e4d27ed78d7bb7a0ab459a79388bee8e5
Tuna <tunaegilmez07@gmail.com>
2022-10-21 20:32:59 +0300

    Menu and Navigation graph Created

--- app/src/main/java/com/tunaegilmez/cardetails/MainActivity.kt
+++ app/src/main/java/com/tunaegilmez/cardetails/MainActivity.kt
@@ -2,10 +2,31 @@ package com.tunaegilmez.cardetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_car, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_car_item){
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment()
+            Navigation.findNavController(this, R.id.fragmentContainerView).navigate(action)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
