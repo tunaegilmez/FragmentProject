1bf68066c614c160c48c5243c4bdd3ef199f4e1c
Tuna <tunaegilmez07@gmail.com>
2022-10-21 23:42:32 +0300

    READ_EXTERNAL_STORAGE permission obtained and gallery accessed

--- app/src/main/java/com/tunaegilmez/cardetails/DetailFragment.kt
+++ app/src/main/java/com/tunaegilmez/cardetails/DetailFragment.kt
@@ -1,10 +1,17 @@
 package com.tunaegilmez.cardetails
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class DetailFragment : Fragment() {
 
@@ -19,6 +26,36 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.saveButton).setOnClickListener {
+            save()
+        }
+
+        view.findViewById<ImageView>(R.id.imageView).setOnClickListener {
+            selectImage()
+        }
+    }
+
+    fun selectImage() {
+        activity?.let {
+            if (ContextCompat.checkSelfPermission(it.applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                // permission denied
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            }else{
+                //permission granted
+                val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI) // galleriye gidip medya'ya ulaşıyor
+                startActivityForResult(galleryIntent, 2)
+            }
+        }
+
+    }
+
+    fun save() {
     }
 
 }
